929b2f7a79e5db07e5b0121a4bdb73e95fcd3a98
wildy-y <1216618738@qq.com>
2015-09-17 21:43:28 +0800

    delete the sorted part ,no need

--- kSumII.cpp
+++ kSumII.cpp
@@ -10,25 +10,18 @@ public:
      * @param A: an integer array.
      * @param k: a positive integer (k <= length(A))
      * @param target: a integer
-     * @return a list of lists of integer 
+     * @return a list of lists of integer
      */
     vector<int> r;
     vector<vector<int>> res;
-    
     vector<vector<int> > kSumII(vector<int> A, int k, int target) {
         // write your code here
         vector<vector<int>> ret;
         int len=A.size();
         if(k>len){return res;}
-        vector<pair<int,int>> num;
-        for(int i=0;i<len;i++){
-            num.push_back(make_pair(A[i],i));
-        }
-        sort(A.begin(), A.end());
         solve(A,k,target,r,0);
         ret =res;
         return ret;
-       
     }
     void solve(vector<int> A,int k,int target,vector<int> &r,int begin){
         if(k==0&&target==0){
@@ -63,5 +56,4 @@ int main(){
 	   }
 	   cout<<endl;
 	}
-	Sleep(1000000);
 }
